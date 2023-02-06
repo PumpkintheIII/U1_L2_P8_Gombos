88699bac10c347cf55008fe384d0fea7a92e06fa
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2023-02-06 14:19:10 +0000

    Iteration 1

--- .ccls-cache/@home@runner@U1L2P8Gombos/main.c
+++ .ccls-cache/@home@runner@U1L2P8Gombos/main.c
@@ -3,9 +3,18 @@
   Sales Tax
   Max Gombos
 */
-#include <stdio.h>
+#include <stdio.h> //import input and output functions
 
-int main(void) {
-  
+int main(void) { //define main function
+  float price1, price2, price3; //initialize variable as float
+  float salesTax; //initialize variable as float
+  salesTax = 0.0625; //sets sales tax variable to sales tax
+
+  printf("Input a price: $"); //asks user to input a price
+  scanf("%f", &price1); //accepts input and formats as float
+
+  price1 = price1 + (price1 * salesTax); //adds sales tax
+
+  printf("Price with sales tax: $%0.2f", price1); //prints output
   return 0;
 }
